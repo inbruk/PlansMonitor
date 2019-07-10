289851a4c879ff94a0eceba2dec74c90b28b5fac
inbruk <inbruk@gmail.com>
2019-07-10 12:04:08 +0300

    перенес строку соединения с БД в конфигурацию, добавил вьюхи к контексту через Query Types, см. https://docs.microsoft.com/en-us/ef/core/modeling/query-types и https://stackoverflow.com/questions/36012616/working-with-sql-views-in-entity-framework-core

---
+++ DataAccessLayer/Manual/ActualPlansMonitorContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccessLayer
+{
+    public class ActualPlansMonitorContext : PlansMonitorContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .Build();
+
+                var connectionString = config.GetConnectionString("PlansMonitorDBOnTheLocalhost");
+                optionsBuilder.UseNpgsql(connectionString);
+            }
+        }
+
+        public DbQuery<VwAudit> VwAudit { get; set; }
+        public DbQuery<VwAuditLog> VwAuditLog { get; set; }
+        public DbQuery<VwCorrectiveAction> VwCorrectiveAction { get; set; }
+        public DbQuery<VwEmailTemplate> VwEmailTemplate { get; set; }
+        public DbQuery<VwFileStorage> VwFileStorage { get; set; }
+        public DbQuery<VwRemark> VwRemark { get; set; }
+        public DbQuery<VwUser> VwUser { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // создаем поддержку представлениям (их прокси классы лежат в каталоге Manual)
+            modelBuilder.Query<VwAudit>().ToView("VwAudit");
+            modelBuilder.Query<VwAuditLog>().ToView("VwAuditLog");
+            modelBuilder.Query<VwCorrectiveAction>().ToView("VwCorrectiveAction");
+            modelBuilder.Query<VwEmailTemplate>().ToView("VwEmailTemplate");
+            modelBuilder.Query<VwFileStorage>().ToView("VwFileStorage");
+            modelBuilder.Query<VwRemark>().ToView("VwRemark");
+            modelBuilder.Query<VwUser>().ToView("VwUser");
+        }
+    }
+}
--- DataAccessLayer/PlansMonitorContext.cs
+++ DataAccessLayer/PlansMonitorContext.cs
@@ -31,8 +31,7 @@ namespace DataAccessLayer
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseNpgsql("Host=localhost;Database=PlansMonitor;Username=postgres;Password=12345");
+                throw new Exception("Конфигурирование PlansMonitorContext должно производиться в потомках, или снаружи !");
             }
         }
 
